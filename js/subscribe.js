bab74c5d5fbb745c2cbc71435428fc35865ca80b
Baffer23 <agusbaffo@gmail.com>
2020-10-31 14:03:31 -0300

    creating js code structure and getting all elements of html

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,5 +1,73 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
+function init(){   
+
+    //Get all form elements
+    const fname = document.getElementById('fname');
+    const email = document.getElementById('email');
+    const pass = document.getElementById('pass');
+    const cpass = document.getElementById('cpass');
+    const age = document.getElementById('age');
+    const pnum = document.getElementById('pnum');
+    const address = document.getElementById('address');
+    const city = document.getElementById('city');
+    const pcode = document.getElementById('pcode');
+    const dni = document.getElementById('dni');
+    const btnSubmit = document.getElementById('btn-submit');
+    const hello = document.getElementById('hello-name')
+
+    //Hello message
+    fname.addEventListener('keyup', function(e){ 
+        hello.innerHTML = e.target.value.toUpperCase();
+    });
+    
+    //Data validation
+    btnSubmit.addEventListener('click',function(e){
+        e.preventDefault();
+        valName(fname.value);
+        valEmail(email.value);
+        valPass(pass.value);
+        valCpass(pass.value, cpass.value);
+        valAge(age.value);
+        valPnum(pnum.value);
+        valAddress(address.value);
+        valCity(city.value);
+        valPcode(pcode.value);
+        valDNI(dni.value);
+    });
+}
+
+function valName(str){
+};
+function valEmail(str){
+};
+function valPass(str){
+};
+function valCpass(p,cp){
+};
+function valAge(str){
+};
+function valPnum(str){
+};
+function valAddress(str){
+};
+function valCity(str){
+};
+function valPcode(str){
+};
+function valDNI(str){
+};
+
+
+
+
+
+
+
+
+
+
+
 /*window.onload = function() {
     //init();
     //doSomethingElse();
@@ -21,12 +89,6 @@ function my_function(e){
 }*/
 
 
-function init(){    
-    let fname = document.getElementById('fname'); 
-    fname.addEventListener('keyup', function runEvent(e){ 
-        document.getElementById('hello-name').innerHTML = e.target.value.toUpperCase();
-    });    
-}
 
 
 
