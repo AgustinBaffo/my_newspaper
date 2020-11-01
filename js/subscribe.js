68f4de82f92dafdb16625520d454a63d2c031363
Baffer23 <agusbaffo@gmail.com>
2020-11-01 19:57:05 -0300

    task 05 completed. Added validation to all form. Added alert on submit. Added linking between index and subscribe html file

--- js/subscribe.js
+++ js/subscribe.js
@@ -37,45 +37,210 @@ function init(){
     fname.addEventListener('blur',function(){
         if(!valName(this.value) && this.value != ''){
             ifname.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
         }
     });
-
+    email.addEventListener('blur',function(){
+        if(!valEmail(this.value) && this.value != ''){
+            iemail.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    pass.addEventListener('blur',function(){
+        if(!valPass(this.value) && this.value != ''){
+            ipass.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    cpass.addEventListener('blur',function(){
+        if(!valCpass(pass.value,this.value) && this.value != ''){
+            icpass.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    age.addEventListener('blur',function(){
+        if(!valAge(this.value) && this.value != ''){
+            iage.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    pnum.addEventListener('blur',function(){
+        if(!valPnum(this.value) && this.value != ''){
+            ipnum.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    address.addEventListener('blur',function(){
+        if(!valAddress(this.value) && this.value != ''){
+            iaddress.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    city.addEventListener('blur',function(){
+        if(!valCity(this.value) && this.value != ''){
+            icity.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    pcode.addEventListener('blur',function(){
+        if(!valPcode(this.value) && this.value != ''){
+            ipcode.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    dni.addEventListener('blur',function(){
+        if(!valDNI(this.value) && this.value != ''){
+            idni.style.visibility = "visible";
+            this.style.backgroundColor = "#F99";
+        }
+    });
+    
     //Clear errors
     fname.addEventListener('focus',function(){
         if( ifname.style.visibility = "visible"){
             ifname.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    email.addEventListener('focus',function(){
+        if( iemail.style.visibility = "visible"){
+            iemail.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    pass.addEventListener('focus',function(){
+        if( ipass.style.visibility = "visible"){
+            ipass.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    cpass.addEventListener('focus',function(){
+        if( icpass.style.visibility = "visible"){
+            icpass.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    age.addEventListener('focus',function(){
+        if( iage.style.visibility = "visible"){
+            iage.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    pnum.addEventListener('focus',function(){
+        if( ipnum.style.visibility = "visible"){
+            ipnum.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    address.addEventListener('focus',function(){
+        if( iaddress.style.visibility = "visible"){
+            iaddress.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    city.addEventListener('focus',function(){
+        if( icity.style.visibility = "visible"){
+            icity.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    pcode.addEventListener('focus',function(){
+        if( ipcode.style.visibility = "visible"){
+            ipcode.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
+        }
+    });
+    dni.addEventListener('focus',function(){
+        if( idni.style.visibility = "visible"){
+            idni.style.visibility = "hidden";
+            this.style.backgroundColor = "initial";
         }
     });
 
     //Submit data validation
     btnSubmit.addEventListener('click',function(e){
+        let sError = 'Invalid: ';
+        let msg = '';
+        let flag_error;
+
         e.preventDefault();
-        valName(fname.value);
-        valEmail(email.value);
-        valPass(pass.value);
-        valCpass(pass.value, cpass.value);
-        valAge(age.value);
-        valPnum(pnum.value);
-        valAddress(address.value);
-        valCity(city.value);
-        valPcode(pcode.value);
-        valDNI(dni.value);
+
+        if(!valName(fname.value)){
+            sError = sError + 'Full Name, ';
+            flag_error = true;
+        }
+        if(!valEmail(email.value)){
+            sError = sError + 'Email, ';
+            flag_error = true;
+        }
+        if(!valPass(pass.value)){
+            sError = sError + 'Password, ';            
+            flag_error = true;
+        }
+        if(!valCpass(pass.value, cpass.value)){
+            sError = sError + "Passwords don't match, ";    
+            flag_error = true;        
+        }
+        if(!valAge(age.value)){
+            sError = sError + 'Age, ';           
+            flag_error = true; 
+        }
+        if(!valPnum(pnum.value)){            
+            sError = sError + 'Phone Number, ';
+            flag_error = true;
+        }
+        if(!valAddress(address.value)){
+            sError = sError + 'Address, ';      
+            flag_error = true;      
+        }
+        if(!valCity(city.value)){
+            sError = sError + 'City, ';      
+            flag_error = true;      
+        }
+        if(!valPcode(pcode.value)){
+            sError = sError + 'Postal Code, ';  
+            flag_error = true;          
+        }
+        if(!valDNI(dni.value)){
+            sError = sError + 'DNI, ';       
+            flag_error = true;     
+        }
         
-        //console.log(valName(fname.value));
-        //console.log(valEmail(email.value));
-        //console.log(valPass(pass.value));
-        //console.log(valCpass(pass.value, cpass.value));
-        //console.log(valAge(age.value));
-        //console.log(valPnum(pnum.value));
-        //console.log(valAddress(address.value));
-        //console.log(valCity(city.value));
-        //console.log(valPcode(pcode.value));
-        //console.log(valDNI(dni.value));
+        msg += 'Full Name: ' + fname.value + '\n'
+        msg += 'Email: ' + email.value + '\n'
+        msg += 'Password: ' + pass.value + '\n'
+        msg += 'Confirm Password: ' + cpass.value + '\n'
+        msg += 'Age: ' + age.value + '\n'
+        msg += 'Phone Number: ' + pnum.value + '\n'
+        msg += 'Address: ' + address.value + '\n'
+        msg += 'City: ' + city.value + '\n'
+        msg += 'Postal Code: ' + pcode.value + '\n'
+        msg += 'DNI: ' + dni.value + '\n'
+
+        if(flag_error){
+            msg += '\n' + sError.slice(0,-2);
+        }else{
+            msg += '\n' + 'Successful Login!'
+        }
+
+        alert(msg);
+
     });
 }
 
 function valName(str){
-    return (str.length >= 7 && str.indexOf(' ') != -1);
+    let re = /^[a-zA-Z\s]+$/
+    if(!(re.test(str))){
+        return false;
+    }
+
+    let a = str.split(' ');
+    a = a.filter(removeSpace);   
+    if (a.length < 2){           //If there are less than 2 elements, it is not: "char-space-char"
+        return false;
+    }
+
+    return a.join('').length >=6;
 };
 function valEmail(str){
     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -86,7 +251,7 @@ function valPass(str){
     return (re.test(str) && str.length >= 8);
 };
 function valCpass(p,cp){
-    return (valPass(p) && p==cp);
+    return (p===cp);
 };
 function valAge(str){
     let re = /^[0-9]+$/
@@ -96,21 +261,34 @@ function valPnum(str){
     let re = /^[0-9]+$/
     return (re.test(str) && str.length >= 7);
 };
-function valAddress(str){
-
+function valAddress(str){    
+    let re = /^[a-zA-Z0-9\s]+$/
+    let re2 = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i   //at least 1 char and 1 number
+    if(!re.test(str)){
+        return false;
+    }
+    let a = str.split(' ');
+    a = a.filter(removeSpace);   
+    if (a.length < 2){              //If there are less than 2 elements, it is not: "char-space-char"
+        return false;
+    }
+    
+    let s = a.join('');
+    return s.length >=5 && re2.test(s);
 };
 function valCity(str){
-    return str.length >= 3;
+    let re = /^[a-zA-Z\s]+$/
+    return re.test(str) && str.length >= 3;
 };
 function valPcode(str){
-    return str.length >= 3;
+    let re = /^[0-9]+$/
+    return re.test(str) && str.length >= 3;
 };
 function valDNI(str){
     let re = /^[0-9]+$/
     return (re.test(str) && str.length >= 7 && str.length <= 8);
 };
-
- /*
-itemInput.addEventListener('focus',runEvent);
-itemInput.addEventListener('blur',runEvent);
- */
+function removeSpace(val){
+    let re = /^[a-zA-Z0-9]+$/
+    return re.test(val) && val != "";
+}
